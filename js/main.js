8b9f7629967802b39698cd13af66610153bd09e7
Artem <artemshilay@gmail.com>
2012-12-27 12:51:27 +0200

    Update js/main.js
    Added basic pattern to wrap a code by anonymous function.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+(function (app, window, $, undefined)
 
+// put your awesome code here
+// use 'app' as root namespace for all your code to keep global namespace clear 
+
+) (window.application = window.application || {}, window, jQuery)
