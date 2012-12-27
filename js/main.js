072ede705e31ce90a83622b6c0416986710dcc0c
Artem <artemshilay@gmail.com>
2012-12-27 12:55:24 +0200

    Update js/main.js
    Clear wrong place commit. 

--- js/main.js
+++ js/main.js
@@ -1,6 +1,1 @@
-(function (app, window, $, undefined)
 
-// put your awesome code here
-// use 'app' as root namespace for all your code to keep global namespace clear 
-
-) (window.application = window.application || {}, window, jQuery)
